26cbc7a7bfdfae15c91f84d2e5cb55a99cd55910
Brian Gonzalez <me@briangonzalez.org>
2016-06-05 13:32:05 -0700

    added in tests

---
+++ tests/test.js
@@ -0,0 +1,35 @@
+
+function run(name) {
+  require('child_process')
+  .execSync(`./index.js start tests/examples/.${name}`, { stdio:[0] });
+}
+
+const chai = require('chai');
+let { assert, expect } = chai;
+
+describe('mertrc-error', function () {
+  this.timeout(20000);
+  it('should throw an error', function () {
+    expect(() => {
+      run('mertrc-error');
+    }).to.throw(Error);
+  });
+});
+
+
+const examples = [
+  'mertrc-base',
+  'mertrc-windows',
+];
+
+examples.forEach((example) => {
+  describe(example, function () {
+    this.timeout(20000);
+    it('should not throw an error', function (done) {
+      expect(() => {
+        run(example);
+        done();
+      }).to.not.throw(Error);
+    });
+  });
+});
